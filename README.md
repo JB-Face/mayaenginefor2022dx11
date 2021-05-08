66b7ffa9d10265d3b61016ad36186971f5f1f4de
JBFace <573765174@qq.com>
2021-05-08 23:27:33 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+mayaenginefor2022dx11
---
+++ src/cmd/ShaderReloadCmd.h
@@ -0,0 +1,25 @@
+﻿//
+// Copyright (c) GANBARION Co., Ltd. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+//
+
+#pragma once
+
+#include "Common.h"
+#include "../pch.h"
+
+
+/**
+ * シェーダ再読み込み用コマンド
+ */
+class ShaderReloadCmd: public MPxCommand
+{
+public:
+	ShaderReloadCmd() {};
+	virtual      ~ShaderReloadCmd() {};
+
+	virtual MStatus doIt(const MArgList& args) override;
+	virtual bool  isUndoable() const override { return false; };
+
+	static void* creator();
+};
